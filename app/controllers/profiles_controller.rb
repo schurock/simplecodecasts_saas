e08f9d8320a4a6300eef7475071853d7d75a6d0e
schurock <t.schurig@gmail.com>
2015-07-05 14:37:34 +0000

    update create action in profiles controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,22 @@
 class ProfilesController < ApplicationController
     def new
-        @user = User.find( params[:user_id])
+        @user = User.find( params[:user_id]) #to identify the unique User
         @profile = @user.build_profile
+    end
+    
+    def create
+        @user = User.find( params[:user_id])
+        @profile = @user.build_profile(profile_params)
+        if @profile.save
+            flash[:success] = "Profile updated!" 
+            redirect_to_user_path( params[:used_id] )
+        else
+            render action: :new
+        end
+    end
+    
+    private #whitelisting of params
+    def profile_params
+        params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
     end
 end
